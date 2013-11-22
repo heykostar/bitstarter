3038c317b07e9d1b0ef0b6157e3cc1d214a27fd1
Ubuntu <ubuntu@ip-172-31-5-171.us-west-2.compute.internal>
2013-11-22 00:51:00 +0000

    git add, update3

--- web.js
+++ web.js
@@ -2,9 +2,12 @@ var express = require('express');
 var app = express();
 app.use(express.logger());
 
+//buf.toString(encoding)
+//fs.readFileSync(filename)
+//buf.toString(fs.readFileSynch(index.html))
+
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
-});
+  response.send(buf.toString(fs.readFileSynch(index.html))) });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
