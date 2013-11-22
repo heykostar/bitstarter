6298c7f93537a9ae9162f9f59ed76b3b9a415b46
Ubuntu <ubuntu@ip-172-31-5-171.us-west-2.compute.internal>
2013-11-22 00:59:42 +0000

    4th edit, 2nd add

--- web.js
+++ web.js
@@ -7,7 +7,12 @@ app.use(express.logger());
 //buf.toString(fs.readFileSynch(index.html))
 
 app.get('/', function(request, response) {
-  response.send(buf.toString(fs.readFileSynch(index.html))) });
+var fs = require('fs');
+var data = fs.readFileSync('index.html');
+  response.send(data.toString('utf-8'));
+});
+
+//response.send(buf.toString(fs.readFileSynch(index.html))) });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
